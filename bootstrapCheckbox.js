16d1f0ccbe4385b7e05ce558f30ec0687e9e4ff0
wboka <boka.wayne@gmail.com>
2015-08-03 21:43:11 -0400

    Updates the demo page and updates the official plugin code.

--- bootstrapCheckbox.js
+++ bootstrapCheckbox.js
@@ -1,44 +1,96 @@
+/**
+ *  @author 		Wayne T Boka
+ *  @lastmodified 	201507291636
+ *  @version		1.0.0
+ *  @description	Plugin for Bootstrap styled checkboxes
+ */
+
 (function ($) {
-    // Define available methods
-    var methods = {
-        // Initialize checkboxes
-        init: function () {
-            return this.each(function () {
-                // Does the this parent element contain a "span.bootstrap-checkbox"
-                if ($(this).find("span.bootstrapCheckbox").length == 0) {
-                    // If not, prepend it the parent element
-                    $(this).prepend("<span class='bootstrapCheckbox glyphicon glyphicon-unchecked'></span>");
-                }
-                // Checked to unchecked and vice versa
-                $(this).on("click", methods['toggle']);
-            });
-        },
-        toggle: function () {
-            // Toggle which bootstrap checkbox glyphicon is showing
-            $(this).find("span.bootstrapCheckbox").toggleClass("glyphicon-check glyphicon-unchecked");
-        },
-        toggleAll: function () {
-            // Toggle the checked/unchecked state for every bootstrap-checkbox in the parent element
-            return this.each(function () {
-                $(this).find("span.bootstrapCheckbox").toggleClass("glyphicon-check glyphicon-unchecked");
-            });
-        },
-        selectAll: function () {
-            // Set all bootstrap-checkboxes in the parent element to checked
-            $(this).find("span.bootstrapCheckbox").removeClass("glyphicon-check glyphicon-unchecked").addClass("glyphicon-check");
-        },
-        deselectAll: function () {
-            // Set all bootstrap-checkboxes in the parent element to unchecked
-            $(this).find("span.bootstrapCheckbox").removeClass("glyphicon-check glyphicon-unchecked").addClass("glyphicon-unchecked");
-        }
-    };
-    
-    $.fn.bootstrapCheckbox = function (method) {
-        // Check if the desired method exists
-        if (methods[method]) {
-            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-        } else {
-            $.error("Method: \"" + method + "\" does not exist in bsCheckbox");
-        }
-    };
+	// Define available methods
+	var methods = {
+		// Initialize checkboxes
+		init: function () {
+			return this.each(function () {
+				// Checked to unchecked and vice versa
+				if ($(this).siblings("span.bootstrapCheckboxSpan").length === 0) {
+					$(this).after("<span class='bootstrapCheckboxSpan glyphicon " + (this.checked ? "glyphicon-check" : "glyphicon-unchecked") + "'></span>");
+				}
+
+				$(this).hide();
+
+				$(this).siblings("span.bootstrapCheckboxSpan, label").on("click", methods['toggle']);
+			});
+		},
+		toggle: function () {
+			// Toggle which bootstrap checkbox glyphicon is showing
+			$bootstrapCheckbox = $(this).is("span.bootstrapCheckboxSpan") ? $(this) : $bootstrapCheckbox = $("span.bootstrapCheckboxSpan", $(this).parent());
+
+			$bootstrapCheckbox.toggleClass("glyphicon-check glyphicon-unchecked");
+
+			$originalCheckbox = $(this).siblings("input.bootstrapCheckbox");
+
+			$bootstrapCheckbox.hasClass("glyphicon-check") ? $originalCheckbox.prop("checked", true) : $originalCheckbox.removeProp("checked");
+
+			return this;
+		},
+		toggleAll: function () {
+			// Toggle the checked/unchecked state for every bootstrapCheckbox
+			return this.each(function () {
+				$bootstrapCheckbox = $(this).is("span.bootstrapCheckboxSpan") ? $(this) : $bootstrapCheckbox = $("span.bootstrapCheckboxSpan", $(this).parent());
+
+				$bootstrapCheckbox.toggleClass("glyphicon-check glyphicon-unchecked");
+
+				$originalCheckbox = $(this).siblings("input.bootstrapCheckbox");
+
+				$bootstrapCheckbox.hasClass("glyphicon-check") ? $originalCheckbox.prop("checked", true) : $originalCheckbox.removeProp("checked");
+			});
+		},
+		selectAll: function () {
+			// Set all bootstrapCheckboxes to checked
+			$bootstrapCheckbox = $(this).is("span.bootstrapCheckboxSpan") ? $(this) : $bootstrapCheckbox = $("span.bootstrapCheckboxSpan", $(this).parent());
+
+			$bootstrapCheckbox.removeClass("glyphicon-check glyphicon-unchecked").addClass("glyphicon-check");
+
+			$(this).siblings("input.bootstrapCheckbox").prop("checked", true);
+
+			return this;
+		},
+		deselectAll: function () {
+			// Set all bootstrapCheckboxes to unchecked
+			$bootstrapCheckbox = $(this).is("span.bootstrapCheckboxSpan") ? $(this) : $bootstrapCheckbox = $("span.bootstrapCheckboxSpan", $(this).parent());
+
+			$bootstrapCheckbox.removeClass("glyphicon-check glyphicon-unchecked").addClass("glyphicon-unchecked");
+
+			$(this).siblings("input.bootstrapCheckbox").removeProp("checked");
+
+			return this;
+		},
+		reset: function() {
+			// Set the bootstrapCheckbox(es) back to the unchecked state
+			return this.each(function() {
+				$bootstrapCheckbox = $(this).is("span.bootstrapCheckboxSpan") ? $(this) : $bootstrapCheckbox = $("span.bootstrapCheckboxSpan", $(this).parent());
+
+				$bootstrapCheckbox.removeClass("glyphicon-check glyphicon-unchecked").addClass("glyphicon-unchecked");
+
+				$(this).siblings("input.bootstrapCheckbox").removeProp("checked");
+			});
+		},
+		destroy: function() {
+			return this.each(function() {
+				$(this).show();
+
+				$(this).siblings("span.bootstrapCheckboxSpan, label").off("click");
+				$(this).siblings("span.bootstrapCheckboxSpan").remove();
+			});
+		}
+	};
+
+	$.fn.bootstrapCheckbox = function (method) {
+		// Check if the desired method exists
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else {
+			$.error("Method: \"" + method + "\" does not exist in bootstrapCheckbox");
+		}
+	};
 })(jQuery);
